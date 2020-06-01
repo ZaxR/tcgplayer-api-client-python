8933617e0900ca2e69ff4af85807255fc4119631
Zax Rosenberg <zax.rosenberg@gmail.com>
2020-06-01 08:58:47 -0500

    Upgrade dynamic method generation

--- tcgplayer_api/client.py
+++ tcgplayer_api/client.py
@@ -67,15 +67,12 @@ class RequestsClient:
 
 
 class TCGPlayerClient:
-    # TODO: parameterize version
-    BASE_URL = "http://api.tcgplayer.com/{api_version}"
-
     def __init__(self, auth: BearerAuth, headers: dict = None,
-                 version: str = None, ):
+                 api_version: str = None, ):
         headers = headers or {}
         self.client = RequestsClient(auth=auth, headers=headers)
-        self.version = version or "v1.37.0"  # _get_latest_api_version()
-        self.base_url = self.BASE_URL.format(api_version=self.version)
+        self.version = api_version or "v1.37.0"  # _get_latest_api_version()
+        self.base_url = f"http://api.tcgplayer.com/{self.version}"
         self.services = {}
 
         api_file = os.path.abspath(os.path.join(os.path.dirname(__file__),
@@ -83,36 +80,37 @@ class TCGPlayerClient:
         with open(api_file, "r") as f:
             api_data = json.load(f)
 
+        # Add each API object as a class instance method
         for service in api_data:
-            service_name = service["name"]
-            uri = service["uri"]
-            http_method = service["http_method"]
-            func_name = words_to_snake_case(service_name)
-            # Call API Method directly as a class method
-            self.__dict__[func_name] = partial(self._call, service_name, uri, http_method)
+            func_name = words_to_snake_case(service["name"])
+            method = self._method_factory(func_name, service["uri"], service["http_method"])
+            method.__name__ = method.__qualname__ = func_name
+            method.__doc__ = service.get("description")
+            self.__dict__[func_name] = method
 
-    def _call(self, service_name, uri, http_method, path_params=None, **query_params):
-        # TODO: change this so path_params doesn't have to be a dict
-        path_params = path_params or {}
+    def _method_factory(self, service_name: str, uri: str, http_method: str):
+        """Factory function to create class instance methods from api_specs."""
+        def service_name(path_params: dict = None, **query_params):
+            path_params = path_params or {}
+            request_uri = uri.format(**path_params)
+            request_url = f"{self.base_url}{request_uri}"
 
-        # Prepare API call parameters
-        request_uri = uri.format(**path_params)
-        request_url = f"{self.base_url}{request_uri}"
+            # Execute API Call
+            # Will raise HTTPError: "405 Client Error: Method Not Allowed for url"
+            # if a bad http_method is given
+            # TODO: See why posts aren't working ATM
+            response = self.client.request(http_method, request_url, params=query_params)
 
-        # Execute API Call
-        # Will raise HTTPError: "405 Client Error: Method Not Allowed for url"
-        # if a bad http_method is given
-        # TODO: See why posts aren't working ATM
-        response = self.client.request(http_method, request_url, params=query_params)
+            if response.status_code != 200:
+                raise UnexpectedStatusCode
 
-        if response.status_code != 200:
-            raise UnexpectedStatusCode
+            resp_json = response.json
+            if not resp_json:
+                raise ValueError("No data returned.")
 
-        resp_json = response.json
-        if not resp_json:
-            raise ValueError("No data returned.")
+            # TODO, loop through pages and collect results
+            # TODO create a class for the response
 
-        # TODO, loop through pages and collect results
-        # TODO create a class for the response
+            return resp_json
 
-        return resp_json
+        return service_name
