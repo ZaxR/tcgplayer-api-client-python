cd37d271bc93d5f42f091a822d9dfd011f6dcf50
ddghunter <ddg.hunter@gmail.com>
2021-04-10 13:52:31 -0500

    added snake/camel case conversions

--- tcgplayer_api/utils.py
+++ tcgplayer_api/utils.py
@@ -1,5 +1,18 @@
-""" """
+""" Basic utility functions """
 
+
+#####################################################################################
+# STRING FUNCTIONS
+#####################################################################################
+
+def camel_to_snake_case(camel_str):
+    return re.sub(r'(?<!^)(?=[A-Z])', '_', camel_str).lower()
+
+def snake_to_camel_case(snake_str):
+    components = snake_str.split('_')
+    # We capitalize the first letter of each component except the first one
+    # with the 'capitalize' method and join them together.
+    return components[0] + ''.join(x.capitalize() for x in components[1:])
 
 def words_to_snake_case(s):
     components = s.split(' ')
